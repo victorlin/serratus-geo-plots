469ad15896f7a973837fa5e4389dafe350ff7840
Victor Lin <victor@victorl.in>
2021-02-25 10:58:31 -0500

    add notebook and other plots

---
+++ aurora.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+endpoint = 'serratus-aurora-20210223.cluster-ro-ccz9y6yshbls.us-east-1.rds.amazonaws.com'
+database = 'summary'
+username = 'public_reader'
+password = 'serratus'
+engine = create_engine(f'postgresql+psycopg2://{username}:{password}@{endpoint}/{database}')
+
+def download(query):
+    with engine.connect() as con:
+        df = pd.read_sql(query, con)
+    return df
